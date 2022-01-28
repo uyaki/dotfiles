372dce90b97079667df3ed20818c2e61c6f3d25d
uyaki <737376326@qq.com>
2022-01-28 22:56:36 +0800

    feat: update ubuntu

--- linux/ubuntu-install.md
+++ linux/ubuntu-install.md
@@ -1,29 +1,44 @@
 ## 使用 ssh连接服务器
-```sh
+```bash
 ssh -o ServerAliveInterval=180 -o ServerAliveCountMax=3 -o KeepAlive=yes [user]@[ip]
 ## 如果你是重装服务器可能还需要重置下ssh
 ssh-keygen -R [ip]
 ```
+## 切换到管理员角色
+```bash
+## 切换到root下
+sudo su
+## `~` => `/root`
+cd ~
+```
 
 ## 安装git、zsh
-```sh
+
+```bash
 sudo apt install -y git zsh
 ```
+
 ## 安装zsh
-```sh
+
+```bash
 ### install `oh-my-zsh`
 sudo sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 ```
 
 ## clone project
-```sh
-sudo git clone https://github.com/uyaki/dotfiles.git /home/ubuntu/dotfiles
+
+```bash
+sudo git clone https://github.com/uyaki/dotfiles.git ~/dotfiles
 ```
 
-> 文件默认操作路径 `/home/ubuntu/dotfiles`
+> 文件默认操作路径 `/root/dotfiles`
 
-```
-cd /home/ubuntu/dotfiles
+> 其实不建议执行脚本，因为执行过程过长，不利于问题及时解决。
+
+> 建议手动复制需要的执行语句执行
+
+```bash
+cd dotfiles
 sudo chmod 777 ./linux/*-install.sh
 ./linux/ubuntu-install.sh
 ```
--- linux/ubuntu-install.sh
+++ linux/ubuntu-install.sh
@@ -1,12 +1,15 @@
 # this script setup my personal ubuntu setup
 
 ## install basic tools, grouped line by line
-### apt 更新可用软件包列表 &&  通过 卸载/安装/升级 来更新系统 && 卸载所有自动安装且不再使用的软件包
-sudo apt update && sudo apt upgrade -y && sudo apt autoremove
-### 安装 ssh vim tmux docker tree
+### apt 更新可用软件包列表 &&  通过 卸载/安装/升级 来更新系统
+sudo apt update && sudo apt upgrade -y
+###  安装git zsh
+sudo apt install -y git zsh
+### 安装 ssh vim tmux docker tree autojump
 sudo apt install -y ssh vim tmux docker.io tree autojump
 ### 安装java
 sudo apt install -y openjdk-8-jdk
+
 ### install Maven
 sudo wget https://dlcdn.apache.org/maven/maven-3/3.8.4/binaries/apache-maven-3.8.4-bin.tar.gz -P /tmp
 sudo tar xf /tmp/apache-maven-*.tar.gz -C /opt
@@ -14,7 +17,7 @@ sudo ln -s /opt/apache-maven-3.8.4 /opt/maven
 
 ## install `oh-my-zsh`
 ### 备份原始配置文件
-sudo cp ~/.zshrc ~/.zshrc_old
+sudo mv ~/.zshrc ~/.zshrc_old
 ### 下载主题
 sudo git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 ### 下载插件
@@ -22,7 +25,10 @@ sudo git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 ### 创建软链接
-sudo ln -s /home/ubuntu/dotfiles/zsh/zshrc-linux.conf ~/.zshrc
-sudo ln -s /home/ubuntu/dotfiles/zsh/.p10k.zsh ~/.p10k.zsh
+sudo ln -s ~/dotfiles/zsh/zshrc-linux.conf ~/.zshrc
+sudo ln -s ~/dotfiles/zsh/.p10k.zsh ~/.p10k.zsh
 ### 激活配置
 source ~/.zshrc
+
+### 非必要的：卸载所有自动安装且不再使用的软件包
+sudo apt autoremove
