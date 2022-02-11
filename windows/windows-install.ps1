19f8a17bb5e8222c2978b969bf67a079fcc11522
uyaki <737376326@qq.com>
2022-02-12 01:57:58 +0800

    docs: update

--- windows/windows-install.ps1
+++ windows/windows-install.ps1
@@ -45,8 +45,8 @@ winget install -e --id JetBrains.IntelliJIDEA.Ultimate
 winget install -e --id Docker.DockerDesktop
 
 ## test
-docker run --name repo alpine/git clone https://github.com/docker/getting-started.git
-docker cp repo:/git/getting-started/ .
-cd .\getting-started\
-docker build -t docker101tutorial .
-docker run -d -p 80:80 \ --name docker-tutorial docker101tutorial
+## docker run --name repo alpine/git clone https://github.com/docker/getting-started.git
+## docker cp repo:/git/getting-started/ .
+## cd .\getting-started\
+## docker build -t docker101tutorial .
+## docker run -d -p 80:80 \ --name docker-tutorial docker101tutorial
